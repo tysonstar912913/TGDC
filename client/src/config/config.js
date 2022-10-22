61c551e7aedcaf998a8a01f69fbd4c6b29a0b186
may79 <princetall.501@gmail.com>
2022-10-22 12:22:25 -0400

    update dApp

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,7 +1,11 @@
-export const CONTRACT_ADDRESS = "0x9A620d782F2CF11D15A153217D1DED60506A3B2a";   // The contract address to Rinkeby test network
+export const CONTRACT_ADDRESS = "0x0f1Cd985bADb3C22330b907E6Ef2da0cFD2137fB";   // The contract address to Rinkeby test network
 export const HTTP_PREFIX = "https";
 export const API_HOST = "thegooddogclub.io";
+export const HOST = `${HTTP_PREFIX}://${API_HOST}/`;
+
+// export const HTTP_PREFIX = "http";
+// export const API_HOST = "192.168.112.74";
 // export const API_PORT = "3000";
 // export const HOST = `${HTTP_PREFIX}://${API_HOST}:${API_PORT}/`;
-export const HOST = `${HTTP_PREFIX}://${API_HOST}/`;
+
 export const VERIFY_WHITELIST_APIURL = `${HOST}api/verifywhitelist`;
